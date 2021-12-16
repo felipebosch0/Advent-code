7284165ca1984b66e0004e94a220b394c46052fd
felipebosch0 <felipebosch0@gmail.com>
2021-12-16 17:00:44 -0300

    modified

---
+++ day-1-part2.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main1() {
+	lines := ReadToLines2(os.Stdin)
+
+	Part2(lines)
+}
+
+// this func read .txt
+func ReadToLines2(rd io.Reader) []string {
+	var result []string
+	reader := bufio.NewReader(rd)
+
+	for {
+		text, err := reader.ReadString('\n')
+
+		if err == io.EOF {
+			break
+		}
+		result = append(result, strings.TrimSuffix(text, "\n"))
+	}
+	return result
+}
+
+func Part2(lines []string) {
+	ejex := 0
+	ejey := 0
+	eje := 0
+
+	for _, v := range lines {
+		order := 0
+		if diagnosis, _ := fmt.Sscanf(v, "forward %d", &order); diagnosis > 0 {
+			ejex += order
+			ejey += eje * order
+		} else if diagnosis, _ := fmt.Sscanf(v, "down %d", &order); diagnosis > 0 {
+			eje += order
+		} else if diagnosis, _ := fmt.Sscanf(v, "up %d", &order); diagnosis > 0 {
+			eje -= order
+		}
+	}
+
+	fmt.Printf("Result: %d\n", ejex*ejey)
+}
